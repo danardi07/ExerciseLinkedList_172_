8be87ce68ddf1df34eb5bb816b4a87e2060e21eb
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2023-06-09 10:37:16 +0700

    circular linked list

--- ExerciseLinkedList_172/ExerciseLinkedList_172.cpp
+++ ExerciseLinkedList_172/ExerciseLinkedList_172.cpp
@@ -1,20 +1,99 @@
-// ExerciseLinkedList_172.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <string>
+using namespace std;
+class Node {
+public:
+	int rollNumber;
+	string name;
+	Node* next;
+};
+class CircularLinkedList {
+private:
+	Node* LAST;
+public:
+	CircularLinkedList() {
+		LAST = NULL;
+	}
+	void addNode();
+	bool search(int rollno, Node** previous, Node** current);
+	bool listEmpty();
+	bool delNode();
+	void traverse();
+};
+void CircularLinkedList::addNode() { 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+bool CircularLinkedList::search(int rollno, Node** previous, Node** current) {
+	*previous = LAST->next;
+	*current = LAST->next;
+	while (*current != LAST) {
+		if (rollno == (*current)->rollNumber) {
+			return true;
+		}
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	if (rollno == LAST->rollNumber) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+bool CircularLinkedList::listEmpty() {
+	return LAST == NULL;
+}
+bool CircularLinkedList::delNode() { 
+	return LAST == NULL;
+}
+void CircularLinkedList::traverse() {
+	if (listEmpty()) {
+		cout << "\nList is empty\n";
+	}
+	else {
+		cout << "\nRecords in the list are:\n";
+		Node* currentNode = LAST->next;
+		while (currentNode != LAST) {
+			cout << currentNode->rollNumber << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+		cout << LAST->rollNumber << " " << LAST->name << endl;
+	}
+}
+int main() {
+	CircularLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all the records in the list" << endl;
+			cout << "4. Exit" << endl;
+			cout << "\nEnter your choice (1-5): ";
+			char ch;
+			cin >> ch;
+			switch (ch) {
+			case '1': {
+				obj.addNode();
+			}
+			case '2': {
+				obj.delNode();
+			}
+			case '3': {
+				obj.traverse();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << e.what() << endl;
+		}
+	}
+	return 0;
+}
